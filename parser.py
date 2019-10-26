87fcb49299b5f5be15bf76f46ef746c0c0b7a046
Henry <16284830+code2exe@users.noreply.github.com>
2019-10-26 12:40:02 +0100

    Added gitignore and made some code changes

--- parser.py
+++ parser.py
@@ -1,12 +1,18 @@
-
 from ftplib import FTP
 from dateutil import parser
 import os
+from configparser import ConfigParser
 from time import sleep
-ftp = FTP('ftp.ncbi.nlm.nih.gov')
+# Login Block
+setup = ConfigParser()
+setup.read('config.ini')
+server = setup['Host']['Domain']
+directory = setup['Host']['Directory']
+ftp = FTP(server)
 ftp.login()
-ftp.cwd('pubmed/updatefiles')
+ftp.cwd(directory)
 
+# Directory listing
 gz = [x for x in ftp.nlst() if x.endswith('.gz')]
 
 for index, all in enumerate(gz):
@@ -16,7 +22,7 @@ for index, all in enumerate(gz):
     print(f"{index} {all} {dae[0]} {dae[1]}")
 
 num = int(input("Select a start-point? "))
-end = int(input("Select an ed-point? "))
+end = int(input("Select an end-point? "))
 if num == int(num):
     if end == int(end):
         newgz = gz[num:end+1]
@@ -27,8 +33,11 @@ if num == int(num):
                 ftp.retrbinary(f'RETR {x}', fp.write)
 #        wb.close()
 ftp.quit()
-sleep(2)
-os.chdir('gz')
+sleep(1)
+
+#OS Commands for parsing and streaming to text files.
+os.mkdir('txt')
+os.chdir('txt')
 os.system('gzip -d *.gz*')
 job = "for i in $(dir); do $(grep 'ArticleId IdType=\"doi\"' $i | awk -F\">\" '{print $2}' | awk -F\"<\" '{print $1}' > $(basename -s .xml $i).txt); done"
 os.system(job)
